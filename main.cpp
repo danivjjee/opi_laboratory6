82a2d0f7a702e2d571c70d0567f976ba0e16c4ae
danivjje <amdcollsisnt@gmail.com>
2025-10-16 10:54:49 +0300

    feat: initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    unsigned int number;
+    cout << "Введіть число: ";
+    cin >> number;
+
+    unsigned int firstDigit = number / 100;
+    unsigned int secondDigit = (number - (100 * firstDigit)) / 10;
+    unsigned int thirdDigit = number % 10;
+
+    bool isTrue = firstDigit < secondDigit && secondDigit < thirdDigit;
+
+    cout << boolalpha << isTrue << endl;
+    cout << (isTrue ? true : false) << noboolalpha << endl;
+
+    return 0;
+}
